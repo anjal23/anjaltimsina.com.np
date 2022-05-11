96e4701cc2a119be6e40abbe5ec327ba49c22c31
anjal23 <83053827+anjal23@users.noreply.github.com>
2022-05-11 09:31:41 +0545

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -4,15 +4,15 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'anjaltimsina.com.np';
-$email_subject = 'New Form Submission';
-$email_body = "User Name: $name.\n".
+$email_from = 'anjaltimsina.com.np'; 
+$email_subject = "New Form Submission";
+$email_body = "User Name: $name.\n".  
                 "User Email: $visitor_email.\n".
-                  "User Subject: $subject.\n".
+                  "Subject: $subject.\n".
                     "User Message: $message.\n";
-$to = 'anjaltimsina23@gmail.com';
+$to = "anjaltimsina23@gmail.com"; 
 $headers= "From: $email_from \r\n";
-$headers . = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 header("Location: contact.html");
